7b63b7cd434c389421048a3fa8aff295a015b85a
terrenceturner <tsturner24@gmail.com>
2021-10-26 08:47:28 -0400

    added character.h

---
+++ character.h
@@ -0,0 +1,31 @@
+//
+// Created by Terrence Turner on 10/26/21.
+//
+
+#ifndef ADVENTUREGAME_CHARACTER_H
+#define ADVENTUREGAME_CHARACTER_H
+//character classes enum
+typedef enum ClassEnum {
+    WIZARD,
+    WARRIOR,
+    CLERIC,
+    ARCHER
+} Class;
+
+//Character stuct
+typedef struct character_struct {
+    Class class;
+    int health;
+    int attack;
+    int defense;
+    Item *itemPtr;	// Linked list
+} Character;
+
+//functions
+Character InitCharacter(Class class, int health, int attack, int defense);
+void PrintItems(Item *itemPtr);
+void PrintCharacter(Character* character);
+void Attack(int attack, Character* character);
+Item* DropItem(const Character* character);
+
+#endif //ADVENTUREGAME_CHARACTER_H
