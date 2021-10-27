c97cbde9159b54afb22be416cf9040f2847c6906
Dillon-Roller <dillonroller.18@gmail.com>
2021-10-27 09:53:37 -0500

    add documentation

--- enemy.h
+++ enemy.h
@@ -4,20 +4,28 @@
 #ifndef ENEMY_H_
 #define ENEMY_H_
 
+/* struct holding information about an enemy */
 typedef struct enemy {
   int health;
   int attack;
   int defense;
   char name[50];
   char desc[250];
-  Item* item;
+  Item* item; //all enemies drop an item upon being defeated
 } Enemy;
 
+//Returns a new enemy with given stats
 Enemy* InitEnemy(int health, int attack, int defense, char* name, char* desc, Item* item);
+//Print information about enemy
 void PrintEnemy(const Enemy* enemy);
+//Deal damage to enemy
 void AttackEnemy(int damage, Enemy* enemy);
+//Return the item held by enemy
 Item* DropItem(const Enemy* emeny);
+//Returns true if enemy health is <= 0 else false
 bool IsEnemyDead(const Enemy* enemy);
+
+//Init enemies with various stats
 Enemy* InitGargoyle(const int level);
 Enemy* InitElemental(const int level);
 Enemy* InitWisp(const int level);
