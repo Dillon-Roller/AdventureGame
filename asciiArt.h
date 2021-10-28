5c6a3b8b03d40aba7c5d53b3f9ca4aef86044ac6
terrenceturner <tsturner24@gmail.com>
2021-10-28 16:04:58 -0400

    added asciiArt.c and asciiArt.h

---
+++ asciiArt.h
@@ -0,0 +1,30 @@
+//
+// Created by Terrence Turner on 10/28/21.
+//
+
+#ifndef ADVENTUREGAME_ASCIIART_H
+#define ADVENTUREGAME_ASCIIART_H
+//functions
+//items
+void printSword();
+void printShield();
+void printBow();
+void printMusket();
+void printArmor();
+
+//dungeon
+void printDungeon();
+
+//enemies
+void printGargoyle();
+void printWisp();
+void printElemental();
+void printBoss();
+
+//enemies in dungeon
+void printGargoyleDungeon();
+void printWispDungeon();
+void printElementalDungeon();
+void printBossDungeon();
+
+#endif //ADVENTUREGAME_ASCIIART_H
